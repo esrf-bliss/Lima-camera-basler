53def006fda322df2a7f61071b90dd0bc603fed0
Arafat Noureddine <noureddine@synchrotron-soleil.fr>
2014-03-18 14:19:33 +0100

    - Throw exception in setAutoGain/getAutoGain if feature not available
    Conflicts:
    	pom.xml

--- src/BaslerCamera.cpp
+++ src/BaslerCamera.cpp
@@ -1354,6 +1354,7 @@ void Camera::getAutoGain(bool& auto_gain) const
         else
         {
             auto_gain = false;
+			THROW_HW_ERROR(Error)<<"GainAuto Parameter is not Available !";			
         }
     }
     catch (GenICam::GenericException &e)
@@ -1375,7 +1376,11 @@ void Camera::setGain(double gain)
     try
     {
         // you want to set the gain, remove autogain
-        setAutoGain(false);
+        if (GenApi::IsAvailable(Camera_->GainAuto))
+        {		
+			setAutoGain(false);
+		}
+		
         if (GenApi::IsWritable(Camera_->GainRaw) && GenApi::IsAvailable(Camera_->GainRaw))
         {
 
@@ -1398,11 +1403,15 @@ void Camera::setGain(double gain)
             Camera_->GainRaw.SetValue(gain_raw);
             DEB_TRACE() << "gain_raw = " << gain_raw;
         }
+		else
+		{
+			THROW_HW_ERROR(Error)<<"GainRaw Parameter is not Available !";
+		}
     }
     catch (GenICam::GenericException &e)
     {
         // Error handling
-        THROW_HW_ERROR(Error) << e.GetDescription();
+        THROW_HW_ERROR(Error) << e.GetDescription();		
     }
 }
 
