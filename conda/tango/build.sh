ee845a923c28274e3de8066c1fdedd65b1ad8014
Samuel Debionne <samuel.debionne@esrf.fr>
2025-08-04 17:36:17 +0200

    Fix tango package not properly installing in site-packages

--- conda/tango/build.sh
+++ conda/tango/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd tango/
-cmake -Bbuild -H. -GNinja -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_FIND_ROOT_PATH=$PREFIX
+cmake -Bbuild -H. -GNinja -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_FIND_ROOT_PATH=$PREFIX -DPython3_FIND_STRATEGY=LOCATION
 cmake --build build --target install
